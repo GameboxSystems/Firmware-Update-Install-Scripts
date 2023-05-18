f302b260f993539837d231cc3850540362a888c1
DESKTOP-F3PPKET\Kyle <kbrennan1523@gmail.com>
2023-05-18 18:49:02 -0400

    Added code to find the latest firmware file and rewrite the impact file to use it instead of hard coded files names

--- STEP 5 - Flash Firmware Update/FlashUpdate.bat
+++ STEP 5 - Flash Firmware Update/FlashUpdate.bat
@@ -1,5 +1,42 @@
 @echo off
+setlocal enabledelayedexpansion
 
+set folder=%~dp0
+set latestFile=
+set latestTimestamp=0
+set impactFile="%~dp0write_mcs.impact"
+
+for /f "delims=" %%a in ('dir /b /a-d /o-d "%folder%\*.mcs" 2^>nul') do (
+    set fileTimestamp=%%~ta
+    set fileTimestamp=!fileTimestamp:~0,14!!fileTimestamp:~17,2!
+    
+    if "!fileTimestamp!" gtr "!latestTimestamp!" (
+        set latestTimestamp=!fileTimestamp!
+        set latestFile=%%a
+    )
+)
+if not defined latestFile (
+    echo No .mcs files found in the directory.
+    exit /b
+)
+
+
+echo setMode -bs > %impactFile%
+echo setMode -bs >> %impactFile%
+echo setMode -bs >> %impactFile%
+echo setMode -bs >> %impactFile%
+echo setCable -port auto >> %impactFile%
+echo Identify -inferir >> %impactFile%
+echo identifyMPM >> %impactFile%
+echo attachflash -position 1 -spi "W25Q64BV" >> %impactFile%
+echo assignfiletoattachedflash -position 1 -file "./%latestFile%" >> %impactFile%
+echo Program -p 1 -dataWidth 1 -spionly -e -v -loadfpga >> %impactFile%
+echo exit >> %impactFile%
+
+
+
+
+@echo Flashing using %latestFile%
 @echo Firmware update about to begin
 @echo Leave device powered on and plugged in until process is complete!!!
 pause
